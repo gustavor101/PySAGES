f638538df6dbde52d3230968f8d750d96f783599
gustavor101 <gustavor101@gmail.com>
2024-07-31 20:53:40 -0500

    Update steered.py
    update method

--- pysages/methods/steered.py
+++ pysages/methods/steered.py
@@ -19,8 +19,8 @@ from jax import numpy as np
 
 from pysages.methods.bias import Bias
 from pysages.methods.core import generalize
-from pysages.utils import JaxArray
-
+#from pysages.utils import JaxArray
+from pysages.typing import JaxArray
 
 class SteeredState(NamedTuple):
     """
